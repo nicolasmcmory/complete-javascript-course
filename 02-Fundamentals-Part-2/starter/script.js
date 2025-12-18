8e1575018fedee5b8673cb3c5fd582c9d257e9c2
nico <nicolasmcmory@gmail.com>
2025-12-18 17:49:40 -0500

    progress

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -69,56 +69,140 @@
 //   return 2037 - birthYear;
 // };
 
-//Calculate average of each team
-const calcAverage = (scores) => {
-  let sum = 0;
-  for (let score of scores) {
-    sum += score;
+// //Calculate average of each team
+// const calcAverage = (scores) => {
+//   let sum = 0;
+//   for (let score of scores) {
+//     sum += score;
+//   }
+
+//   const avg = sum / scores.length;
+
+//   return avg;
+// };
+
+// const checkWinner = function (t1Name, avgScore1, t2Name, avgScore2) {
+//   let results = {
+//     winnerName: "",
+//     winnerScore: 0,
+//     looserName: "",
+//     looserScore: 0,
+//   };
+//   if (avgScore1 >= avgScore2 * 2) {
+//     results.winnerName = t1Name;
+//     results.looserName = t2Name;
+//     results.winnerScore = avgScore1;
+//     results.looserScore = avgScore2;
+//   } else if (avgScore2 >= avgScore1 * 2) {
+//     results.winnerName = t2Name;
+//     results.looserName = t1Name;
+//     results.winnerScore = avgScore2;
+//     results.looserScore = avgScore1;
+//   }
+//   return results;
+// };
+
+// //Print winning team
+// function printWinner(t1Name, scoresT1, t2Name, scoresT2) {
+//   const results = checkWinner(t1Name,
+//     calcAverage(scoresT1),t2Name,
+//     calcAverage(scoresT2)
+//   );
+
+//   let statement = `${results.winnerName}s win (${results.winnerScore} vs ${results.looserScore})`;
+
+//   if (results.winnerScore == 0) {
+//     statement = "No team wins..";
+//   }
+
+//   return statement;
+// }
+
+// const scoreDolphins = [300, 23, 71]
+// const scoreKoalas = [65, 54, 49]
+
+// console.log(printWinner("Dolphins", scoreDolphins , "Koalas", scoreKoalas));
+
+//Tip calculator based on bill value
+// const tipCalculator = function (bill) {
+//   return bill >= 50 || bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+// //Print values to console
+// function print(result) {
+//   console.log(
+//     `The bill was ${result.bill}, the tip was ${result.tip}, and the total value ${result.total}.`
+//   );
+// }
+
+// const calculator = (bills) => {
+//   let results = new Array();
+//   for (let bill of bills) {
+//     let tip = tipCalculator(bill);
+//     let result = {
+//       bill: bill,
+//       tip: tip,
+//       total: bill + tip,
+//     };
+
+//     print(result);
+//   }
+
+//   return results;
+// };
+
+// const bills = new Array(275, 300, 450);
+// const tips = calculator(bills);
+
+// const person = {
+//   name: "dude",
+//   year: 1992,
+//   profession: "Teacher",
+//   driverLicence: true,
+//   age: null,
+//   calcAge: function () {
+//     this.age = 2037 - this.year;
+//     return this.age;
+//   },
+//   getSummary: function () {
+//     let driverYesNo = this.driverLicence ? "has" : "doesn't have";
+//     this.summary = `${this.name} is a ${this.calcAge()} old ${
+//       this.profession
+//     }, and they ${driverYesNo} a driver's license.`;
+//   },
+// };
+
+// // person.calcAge()
+// person.getSummary();
+// person.age;
+
+// console.log(person.summary);
+// console.log(person.age);
+
+class Person {
+  constructor(firstName, lastName, birthYear, weight, height) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.weight = weight;
+    this.height = height;
   }
 
-  const avg = sum / scores.length;
-  
-  return avg;
-};
-
-const checkWinner = function (t1Name, avgScore1, t2Name, avgScore2) {
-  let results = {
-    winnerName: "",
-    winnerScore: 0,
-    looserName: "",
-    looserScore: 0,
-  };
-  if (avgScore1 >= avgScore2 * 2) {
-    results.winnerName = t1Name;
-    results.looserName = t2Name;
-    results.winnerScore = avgScore1;
-    results.looserScore = avgScore2;
-  } else if (avgScore2 >= avgScore1 * 2) {
-    results.winnerName = t2Name;
-    results.looserName = t1Name;
-    results.winnerScore = avgScore2;
-    results.looserScore = avgScore1;
-  }
-  return results;
-};
-
-//Print winning team
-function printWinner(t1Name, scoresT1, t2Name, scoresT2) {
-  const results = checkWinner(t1Name,
-    calcAverage(scoresT1),t2Name,
-    calcAverage(scoresT2)
-  );
-
-  let statement = `${results.winnerName}s win (${results.winnerScore} vs ${results.looserScore})`;
-
-  if (results.winnerScore == 0) {
-    statement = "No team wins..";
+  calcBMI() {
+    const BMI = this.weight / this.height ** 2;
+    return BMI;
   }
 
-  return statement;
+  print() {
+    console.log(
+      `${this.firstName} was born in ${this.birthYear}, weighs ${
+        this.weight
+      } kgs, measures ${this.height} m and has as BMI of ${this.calcBMI()}`
+    );
+  }
 }
 
-const scoreDolphins = [300, 23, 71]
-const scoreKoalas = [65, 54, 49]
+const john = new Person("John", "Elliot", 1985, 90, 1.8);
 
-console.log(printWinner("Dolphins", scoreDolphins , "Koalas", scoreKoalas));
+debugger;
+john.print();
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,46 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+*/
+
+const data1 = [17, 21, 23];
+
+const printForecast = function (arr) {
+  let str = "...";
+  for (let i = 0; i < arr.length; i++) {
+    str += ` ${arr[i]}°C in ${i + 1} days ...`;
+  }
+  str = str.slice(0, -4); // Remove trailing " ... "
+  console.log(str);
+};
+
+printForecast(data1);
+
+function forecast(temps) {
+  let index = 0; //starting index
+  let message = "";
+  temps.forEach((temp) => {
+    const day = index + 1;
+    const snippet = `${temp}degC in ${day} days`;
+    if (index < temps.length - 1) {
+      message += snippet + " ... ";
+    } else {
+      message += snippet;
+    }
+    index++;
+  });
+
+  return message;
+}
+
+console.log(forecast(data1));
