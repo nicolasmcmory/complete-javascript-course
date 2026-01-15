eb3c01815635b596f37040f2fe0eabaf327af330
nico <nicolasmcmory@gmail.com>
2026-01-15 18:44:58 -0500

    Comment out example HTML and JavaScript code for improved clarity

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -29,7 +29,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -44,5 +43,367 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (indexStart, indexMain) {
+    return [this.starterMenu[indexStart], this.mainMenu[indexMain]];
+  },
+  orderDelivery: function ({
+    indexStart = 1,
+    indexMain = 0,
+    time = '22:00',
+    address = 'none',
+  }) {
+    console.log(
+      this.starterMenu[indexStart],
+      this.mainMenu[indexMain],
+      time,
+      address
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
+// const ingredients = [
+//   prompt('Ingredient 1:'),
+//   prompt('Ingredient 2:'),
+//   prompt('Ingredient 3:'),
+// ];
+
+// console.log(ingredients);
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = {foundedYear: 1980, ...restaurant, founder: 'mario'};
+console.log(newRestaurant);
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = "ristorante roma";
+
+console.log(restaurant.name);
+console.log(restaurantCopy.name);
+
+// restaurant.orderDelivery({
+//   // time: '22:30',
+//   // address: 'address',
+//   // indexMain: 2,
+//   indexStart: 2,
+// });
+
+// Spread operator
+// const arr = [7, 8, 9];
+// const arr2 = [1, 2, ...arr];
+// console.log(arr2);
+// // Or using concat (faster for large arrays)
+// const arr3 = arr.concat(arr2);
+// console.log(arr3);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// console.log(menu);
+
+// // Destructing objects
+// const { name, openingHours, categories } = restaurant;
+
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+
+// console.log(restaurantName, hours, tags);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// console.log(menu, starters);
+
+// // Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+
+// console.log(a, b);
+
+// // Nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+// let [main, , secondary] = restaurant.categories;
+
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+
+// console.log(starter, mainCourse);
+
+// // Deconstructing nested arrays
+// const tdArr = [1, 2, [3, 4]];
+// const [i, , [j, k]] = tdArr;
+// console.log(i, j, k);
+
+// // Setting defaults in array deconstructing
+// const [p = null, q = null, r = null] = [8, 9];
+// console.log(p, q, r);
+
+// const books = [
+//   {
+//     title: 'Algorithms',
+//     author: ['Robert Sedgewick', 'Kevin Wayne'],
+//     publisher: 'Addison-Wesley Professional',
+//     publicationDate: '2011-03-24',
+//     edition: 4,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'algorithms',
+//       'data structures',
+//       'java',
+//       'math',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 976,
+//     format: 'hardcover',
+//     ISBN: '9780321573513',
+//     language: 'English',
+//     programmingLanguage: 'Java',
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.41,
+//         ratingsCount: 1733,
+//         reviewsCount: 63,
+//         fiveStarRatingCount: 976,
+//         oneStarRatingCount: 13,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Structure and Interpretation of Computer Programs',
+//     author: [
+//       'Harold Abelson',
+//       'Gerald Jay Sussman',
+//       'Julie Sussman (Contributor)',
+//     ],
+//     publisher: 'The MIT Press',
+//     publicationDate: '2022-04-12',
+//     edition: 2,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'javascript',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 640,
+//     format: 'paperback',
+//     ISBN: '9780262543231',
+//     language: 'English',
+//     programmingLanguage: 'JavaScript',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.36,
+//         ratingsCount: 14,
+//         reviewsCount: 3,
+//         fiveStarRatingCount: 8,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Computer Systems: A Programmer's Perspective",
+//     author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+//     publisher: 'Prentice Hall',
+//     publicationDate: '2002-01-01',
+//     edition: 1,
+//     keywords: [
+//       'computer science',
+//       'computer systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'engineering',
+//     ],
+//     pages: 978,
+//     format: 'hardcover',
+//     ISBN: '9780130340740',
+//     language: 'English',
+//     programmingLanguage: 'C',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.44,
+//         ratingsCount: 1010,
+//         reviewsCount: 57,
+//         fiveStarRatingCount: 638,
+//         oneStarRatingCount: 16,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Operating System Concepts',
+//     author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+//     publisher: 'John Wiley & Sons',
+//     publicationDate: '2004-12-14',
+//     edition: 10,
+//     keywords: [
+//       'computer science',
+//       'operating systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'Java',
+//       'engineering',
+//     ],
+//     pages: 921,
+//     format: 'hardcover',
+//     ISBN: '9780471694663',
+//     language: 'English',
+//     programmingLanguage: 'C, Java',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 3.9,
+//         ratingsCount: 2131,
+//         reviewsCount: 114,
+//         fiveStarRatingCount: 728,
+//         oneStarRatingCount: 65,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Engineering Mathematics',
+//     author: ['K.A. Stroud', 'Dexter J. Booth'],
+//     publisher: 'Palgrave',
+//     publicationDate: '2007-01-01',
+//     edition: 14,
+//     keywords: ['mathematics', 'engineering'],
+//     pages: 1288,
+//     format: 'paperback',
+//     ISBN: '9781403942463',
+//     language: 'English',
+//     programmingLanguage: null,
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.35,
+//         ratingsCount: 370,
+//         reviewsCount: 18,
+//         fiveStarRatingCount: 211,
+//         oneStarRatingCount: 6,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'The Personal MBA: Master the Art of Business',
+//     author: 'Josh Kaufman',
+//     publisher: 'Portfolio',
+//     publicationDate: '2010-12-30',
+//     keywords: ['business'],
+//     pages: 416,
+//     format: 'hardcover',
+//     ISBN: '9781591843528',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.11,
+//         ratingsCount: 40119,
+//         reviewsCount: 1351,
+//         fiveStarRatingCount: 18033,
+//         oneStarRatingCount: 1090,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Crafting Interpreters',
+//     author: 'Robert Nystrom',
+//     publisher: 'Genever Benning',
+//     publicationDate: '2021-07-28',
+//     keywords: [
+//       'computer science',
+//       'compilers',
+//       'engineering',
+//       'interpreters',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 865,
+//     format: 'paperback',
+//     ISBN: '9780990582939',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.7,
+//         ratingsCount: 253,
+//         reviewsCount: 23,
+//         fiveStarRatingCount: 193,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Deep Work: Rules for Focused Success in a Distracted World',
+//     author: 'Cal Newport',
+//     publisher: 'Grand Central Publishing',
+//     publicationDate: '2016-01-05',
+//     edition: 1,
+//     keywords: ['work', 'focus', 'personal development', 'business'],
+//     pages: 296,
+//     format: 'hardcover',
+//     ISBN: '9781455586691',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.19,
+//         ratingsCount: 144584,
+//         reviewsCount: 11598,
+//         fiveStarRatingCount: 63405,
+//         oneStarRatingCount: 1808,
+//       },
+//     },
+//     highlighted: true,
+//   },
+// ];
+
+// const [firstBook, secondBook] = books;
+
+// console.log(firstBook.title, secondBook.title);
+
+// const [, , thirdBook] = books;
+
+// console.log(thirdBook.title);
+
+// const ratings = [
+//   ['rating', 4.19],
+//   ['ratingsCount', 144584],
+// ];
+
+// const [[, rating], [, ratingsCount]] = ratings;
+
+// console.log(`rating: ${rating}\nratingsCount: ${ratingsCount}`);
+
+// const ratingStars = [63405, 1808];
+
+// const [fiveStarRating, oneStartRating, threeStarRating = 0] = ratingStars;
+
+// console.log(fiveStarRating, oneStartRating, threeStarRating);
