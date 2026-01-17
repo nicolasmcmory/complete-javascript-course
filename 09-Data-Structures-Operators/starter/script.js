a4146cb2f35467dc191914d75c1ce10f9c5dd0b7
nico <nicolasmcmory@gmail.com>
2026-01-17 18:02:59 -0500

    progress

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const weekdays = ['mon', 'tu', 'we', 'thur', 'fri', 'sat', 'sun'];
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -22,6 +24,21 @@ const mexicanFoods = new Set([
   'garlic',
 ]);
 
+const openingHours = {
+  [weekdays[0]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[1]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[2]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -29,24 +46,11 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (indexStart, indexMain) {
+  openingHours,
+  order(indexStart, indexMain) {
     return [this.starterMenu[indexStart], this.mainMenu[indexMain]];
   },
-  orderDelivery: function ({
+  orderDelivery({
     indexStart = 1,
     indexMain = 0,
     time = '22:00',
@@ -56,28 +60,58 @@ const restaurant = {
       this.starterMenu[indexStart],
       this.mainMenu[indexMain],
       time,
-      address
+      address,
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Ingredients: ${ing1}, ${ing2}, ${ing3}`);
   },
-  orderPizza: function (mainIngredient, ...otherIng) {
+  orderPizza(mainIngredient, ...otherIng) {
     console.log(`${mainIngredient}`);
     console.log(`${otherIng}`);
   },
 };
 
-const restaurant1 = {
-  name: 'Capri',
-  // numGuests: 20,
-  numGuests: 0,
-};
+const properties = Object.keys(openingHours);
+console.log(properties);
 
-const restaurant2 = {
-  name: 'Kola',
-  owner: 'Giovani Rossi',
-};
+for (const day of Object.keys(openingHours)) {
+  console.log(day);
+}
+
+// const restaurant1 = {
+//   name: 'Capri',
+//   // numGuests: 20,
+//   numGuests: 0,
+// };
+
+// const restaurant2 = {
+//   name: 'Kola',
+//   owner: 'Giovani Rossi',
+// };
+
+// Optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// Nullish coalescing and optional chaining together, used to check if objects, methods and arrays exist
+// for (const day of weekdays) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day} we are ${open}`);
+// }
+
+// // Optional chaining on methods and nullish coalescing operator
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'method does not exist');
+
+// // On arrays
+// const users = [{ name: 'jonas', email: 'email@email.com' }];
+// console.log(users[0]?.name ?? 'user does not exist');
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i} : ${el}`);
+// }
 
 // // OR assignment operator, assigns concrete value to falsy values
 // restaurant1.numGuests = restaurant1.numGuests || 10;
@@ -86,8 +120,8 @@ const restaurant2 = {
 // restaurant2.numGuests ||= 10
 
 // Nullish assignment operator
-restaurant1.numGuests ??= 10;
-restaurant2.numGuests ??= 10;
+// restaurant1.numGuests ??= 10;
+// restaurant2.numGuests ??= 10;
 
 // console.log(restaurant1.numGuests);
 // console.log(restaurant2.numGuests);
@@ -96,11 +130,11 @@ restaurant2.numGuests ??= 10;
 // restaurant1.owner = restaurant1.owner && 'Anonymous'
 // restaurant2.owner = restaurant2.owner && 'Anonymous'
 
-restaurant1.owner &&= 'Anonymous';
-restaurant2.owner &&= 'Anonymous';
+// restaurant1.owner &&= 'Anonymous';
+// restaurant2.owner &&= 'Anonymous';
 
-console.log(restaurant1);
-console.log(restaurant2);
+// console.log(restaurant1);
+// console.log(restaurant2);
 
 // // Nullish coalescing operator, only null or undefined
 // restaurant.numGuests = 0;
@@ -531,98 +565,98 @@ null
 undefined
 NaN */
 
-/* 
-We're building a football betting app (soccer for my American friends 😅)!
+// /*
+// We're building a football betting app (soccer for my American friends 😅)!
 
-Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
+// */
 
-const team1 = {
-  name: 'team1',
-  players: ['timiy', 'judas', 'larry', 'paul'],
-};
-const team2 = {
-  name: 'team1',
-  players: ['lema', 'orion', 'pauline', 'gertha'],
-};
+// const team1 = {
+//   name: 'team1',
+//   players: ['timiy', 'judas', 'larry', 'paul'],
+// };
+// const team2 = {
+//   name: 'team1',
+//   players: ['lema', 'orion', 'pauline', 'gertha'],
+// };
 
-const [gk, ...fieldPlayers] = team1.players;
-const allPlayers = [...team1.players, ...team2.players];
+// const [gk, ...fieldPlayers] = team1.players;
+// const allPlayers = [...team1.players, ...team2.players];
 
-const playersOneFinal = [...team1.players, 'Thiago', 'Coutinho', 'Perisic'];
+// const playersOneFinal = [...team1.players, 'Thiago', 'Coutinho', 'Perisic'];
 
-console.log(gk, allPlayers, playersOneFinal);
+// console.log(gk, allPlayers, playersOneFinal);
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const { team1: t1, x: draw, team2: t2 } = game.odds;
-console.log(t1, draw, t2);
+// const { team1: t1, x: draw, team2: t2 } = game.odds;
+// console.log(t1, draw, t2);
 
-function printGoals(...players) {
-  let sum = 0;
-  for (const player in players) {
-    sum++;
-  }
-  console.log(...players, `Total: ${sum}`);
-}
+// function printGoals(...players) {
+//   let sum = 0;
+//   for (const player in players) {
+//     sum++;
+//   }
+//   console.log(...players, `Total: ${sum}`);
+// }
 
-printGoals(...team1.players);
+// printGoals(...team1.players);
 
-// Using OR logical operator
-console.log(game.odds.team2 < game.odds.team1 || game.team1);
+// // Using OR logical operator
+// console.log(game.odds.team2 < game.odds.team1 || game.team1);
 
-// Using AND logical operator
-console.log(game.odds.team1 < game.odds.team2 && game.team1);
-// Or
-game.odds.team1 < game.odds.team2 && console.log(`${game.team1} wins.`);
+// // Using AND logical operator
+// console.log(game.odds.team1 < game.odds.team2 && game.team1);
+// // Or
+// game.odds.team1 < game.odds.team2 && console.log(`${game.team1} wins.`);
