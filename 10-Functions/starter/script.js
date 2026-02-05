2ec8d826a214941a93bf32837b1727231ff259a5
nicolasmcmory <nicolasmcmory@gmail.com>
2026-02-04 23:30:30 -0500

    Add poll app implementation with registration and result display functionality

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -51,30 +51,170 @@
 // newPassport(jonas);
 // checkIn(flight, jonas);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// const high5 = function () {
+//   console.log('👋');
+// };
+
+// document.body.addEventListener('click', high5);
+
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greetArrow = greeting => name => console.log(greeting + ' ' + name);
+
+// const greeting = greet('hey there');
+
+// greeting('jonas');
+// greeting('steven');
+// greet('hello')('jonas');
+
+// // const greetArrowUse = greetArrow('hey');
+// greetArrow('hey')('dude');
+
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, passengerName) {
+//     console.log(
+//       `${passengerName} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}` });
+//   },
+// };
+
+// lufthansa.book(239, 'Jonas Nana');
+// console.log(...lufthansa.bookings);
+
+// const eurowings = {
+//   name: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// const book = lufthansa.book;
+
+// book.call(eurowings, 23 ,'Someone')
+// console.log(eurowings)
+
+// book.call(lufthansa, 239, 'marie poppins')
+// console.log(lufthansa)
+
+// const bookEW = book.bind(lufthansa)
+// bookEW(23, 'steven williams')
+
+// const bookEW23 = book.bind(lufthansa,23)
+
+// bookEW23('dude')
+
+// lufthansa.planes = 300
+
+// lufthansa.buyPlane = function(){
+//   this.planes++
+//   console.log(this.planes);
+// }
+
+// document.querySelector('.buy').addEventListener('click', () => lufthansa.buyPlane());
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+//   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+//         What is your favourite programming language?
+//         0: JavaScript
+//         1: Python
+//         2: Rust
+//         3: C++
+//         (Write option number)
+// 1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+const poll = {
+  choices: [
+    ['Javascript', 0],
+    ['Python', 0],
+    ['Rust', 0],
+    ['C++', 0],
+  ],
+  registerNewAnswer() {
+    let question = 'What is your favourite programming language?\n';
+
+    this.choices.forEach((choice, i) => {
+      question += i + ': ' + choice[0] + '\n';
+    });
+
+    let result = Number(prompt(question));
+    this.type = String(prompt('What type?')) || this.type;
+
+    while (!result || result > this.choices.length || result <= 0) {
+      result = Number(prompt(question));
+      this.type = String(prompt('What type?')) || this.type;
+    }
+    this.choices[result][1]++;
+    return this.displayResults();
+  },
+  type: 'array',
+  displayResults() {
+    const scores = [...this.choices].map(([language, score]) => score);
+
+    if (this.type == 'string') {
+      console.log(`Poll results are: ${[...scores].join(", ")}`);
+    } else if (this.type == 'array') {
+      console.log(...this.choices);
+    }
+  },
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// 2. Call this method whenever the user clicks the "Answer poll" button.
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
-
-const high5 = function () {
-  console.log('👋');
-};
-
-document.body.addEventListener('click', high5);
-
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+// 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
