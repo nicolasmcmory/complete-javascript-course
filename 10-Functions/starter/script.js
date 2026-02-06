a1be1e438b9c0c160de10d047e89b39113de4dcd
nicolasmcmory <nicolasmcmory@gmail.com>
2026-02-06 14:06:47 -0500

    Add initial styles.css file for styling the functions project

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -170,51 +170,101 @@ GOOD LUCK 😀
 //         3: C++
 //         (Write option number)
 // 1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
-const poll = {
-  choices: [
-    ['Javascript', 0],
-    ['Python', 0],
-    ['Rust', 0],
-    ['C++', 0],
-  ],
-  registerNewAnswer() {
-    let question = 'What is your favourite programming language?\n';
+// const poll = {
+//   choices: [
+//     ['Javascript', 0],
+//     ['Python', 0],
+//     ['Rust', 0],
+//     ['C++', 0],
+//   ],
+//   registerNewAnswer() {
+//     let question = 'What is your favourite programming language?\n';
 
-    this.choices.forEach((choice, i) => {
-      question += i + ': ' + choice[0] + '\n';
-    });
+//     this.choices.forEach((choice, i) => {
+//       question += i + ': ' + choice[0] + '\n';
+//     });
 
-    let result = Number(prompt(question));
-    this.type = String(prompt('What type?')) || this.type;
+//     let result = Number(prompt(question));
+//     this.type = String(prompt('What type?')) || this.type;
 
-    while (!result || result > this.choices.length || result <= 0) {
-      result = Number(prompt(question));
-      this.type = String(prompt('What type?')) || this.type;
-    }
-    this.choices[result][1]++;
-    return this.displayResults();
-  },
-  type: 'array',
-  displayResults() {
-    const scores = this.choices.map(([language, score]) => score);
+//     while (!result || result > this.choices.length || result <= 0) {
+//       result = Number(prompt(question));
+//       this.type = String(prompt('What type?')) || this.type;
+//     }
+//     this.choices[result][1]++;
+//     return this.displayResults();
+//   },
+//   type: 'array',
+//   displayResults() {
+//     const scores = this.choices.map(([language, score]) => score);
 
-    if (this.type == 'string') {
-      console.log(`Poll results are: ${scores.join(", ")}`);
-    } else if (this.type == 'array') {
-      console.log(...this.choices);
-    }
-  },
-};
+//     if (this.type == 'string') {
+//       console.log(`Poll results are: ${scores.join(", ")}`);
+//     } else if (this.type == 'array') {
+//       console.log(...this.choices);
+//     }
+//   },
+// };
 
 // 2. Call this method whenever the user clicks the "Answer poll" button.
 // document
 //   .querySelector('.poll')
 //   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
 // 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 
 // 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+///////////////////////////////////////////
+// IIFE
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+// h();
+f();
+console.dir(f)
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
