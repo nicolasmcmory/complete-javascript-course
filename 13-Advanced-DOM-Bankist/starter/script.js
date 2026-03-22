67aaed0b96b8d6c0a26fa5139bc9e9387a5fdc9e
nicolasmcmory <nicolasmcmory@gmail.com>
2026-03-22 17:25:03 -0400

    progress

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,31 +1,93 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
-
+// Global variables
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+// Business logic
+// Modal window
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = function (e) {
+  e.preventDefault();
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// Event handlers
+const controlModal = function () {
+  btnsOpenModal.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal(e);
+    }
+  });
+};
+
+// Drivers
+// Main driver function
+function init() {
+  controlModal();
+}
+
+init();
+
+//////////////////////////////////
+// Lectures
+
+// Element builder function
+/* const el = function (tag: string, { cls: string, text: string, children }) {
+  const node = document.createElement(tag);
+  if (cls) node.classList.add(cls);
+  if (text) node.textContent = text;
+  if (children) {
+    children.forEach(child => node.append(child));
   }
+  return node;
+};
+
+const element = el('div', {
+  cls: 'cookie-message',
+  text: 'Cookies are here.',
+  children: [
+    el('button', {
+      cls: 'btn',
+      text: 'Accept',
+    }),
+  ],
+}); */
+
+// document.querySelector('.header').append(element);
+// Templates
+const tplCookieMessage = document
+  .querySelector('template#cookie-tpl')
+  .content.cloneNode(true);
+
+// DOM Objs
+const header = document.querySelector('.header');
+const cookieMessage = tplCookieMessage.querySelector('.cookie-message');
+const acceptCookiesBtn = cookieMessage.querySelector('button');
+
+// Text manipulation
+cookieMessage.querySelector('p').textContent = 'help';
+acceptCookiesBtn.textContent = 'press';
+
+// Handlers
+acceptCookiesBtn.addEventListener('click', e => {
+  cookieMessage.remove();
 });
+
+header.append(tplCookieMessage);
