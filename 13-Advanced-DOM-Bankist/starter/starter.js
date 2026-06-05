b425bfbcdceb91d24b9b093245b4e8b40aa5a47d
nicolasmcmory <nicolasmcmory@gmail.com>
2026-06-05 23:45:43 +0200

    progress

--- 13-Advanced-DOM-Bankist/starter/starter.js
+++ 13-Advanced-DOM-Bankist/starter/starter.js
@@ -243,71 +243,102 @@ imgTargets.forEach(imgTarget => imgObserver.observe(imgTarget)); */
 // Slider component
 // Arrows - Add/remove css classes
 // Get elements from DOM
-const leftArrow = document.querySelector('.slider__btn.slider__btn--left');
-const rightArrow = document.querySelector('.slider__btn.slider__btn--right');
-const slides = document.querySelectorAll('.slide');
-const dotsContainer = document.querySelector('.dots');
+function slider() {
+  const leftArrow = document.querySelector('.slider__btn.slider__btn--left');
+  const rightArrow = document.querySelector('.slider__btn.slider__btn--right');
+  const slides = document.querySelectorAll('.slide');
+  const dotsContainer = document.querySelector('.dots');
+  const activeDotClass = 'dots__dot--active';
 
-const maxSlide = slides.length; // Maximum slide index
-let curSlide = 0; // Current slide index
+  // Global variables for slider component
+  const maxSlideIndex = slides.length - 1; // Maximum slide index
+  let curSlideIndex = 0; // Current slide index
 
-// Slide movement function
-const moveSlides = function (position) {
-  console.log(curSlide);
-  if (Math.abs(curSlide) === maxSlide) curSlide = 0;
+  // Create dots for each slide
+  const createDots = function () {
+    slides.forEach((_, i) =>
+      dotsContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`,
+      ),
+    );
+  };
 
-  // Move slides to the left or right depending on the position argument
-  slides.forEach((slide, i) => {
-    switch (position) {
-      case 'start':
-        slide.style.transform = `translateX(${100 * i}%)`;
-        break;
-      case 'right':
-        slide.style.transform = `translateX(${100 * (i - curSlide)}%)`;
-        break;
-      case 'left':
-        slide.style.transform = `translateX(${100 * (i + curSlide)}%)`;
-        break;
-    }
+  // Slide control functions
+  // Activate dot corresponding to current slide and deactivate all other dots
+  const activateDot = function () {
+    const dots = dotsContainer.querySelectorAll('.dots__dot');
+    if (!dots) return;
+
+    // Deactivate all dots
+    dots.forEach(dot => dot.classList.remove(activeDotClass));
+
+    // Activate dot corresponding to current slide index, current slide
+    const activeDot = dotsContainer.querySelector(
+      `.dots__dot[data-slide="${curSlideIndex}"]`,
+    );
+    if (activeDot) activeDot.classList.add(activeDotClass);
+  };
+
+  // Slide control function - translate each slide by 100% of its width multiplied by the difference between its index and the current slide index
+  const goToSlide = function () {
+    console.log(curSlideIndex);
+    slides.forEach(
+      (slide, i) =>
+        (slide.style.transform = `translateX(${100 * (i - curSlideIndex)}%)`),
+    );
+
+    // Activate dot corresponding to current slide and deactivate all other dots
+    activateDot();
+  };
+
+  // Slide control functions for arrows - increment/decrement current slide index and call goToSlide function, with wrap-around logic
+  const slideRight = function () {
+    if (curSlideIndex === maxSlideIndex) curSlideIndex = 0;
+    else curSlideIndex++;
+    goToSlide();
+  };
+
+  // Slide control function for left arrow - decrement current slide index and call goToSlide function, with wrap-around logic
+  const slideLeft = function () {
+    if (curSlideIndex === 0) curSlideIndex = maxSlideIndex;
+    else curSlideIndex--;
+    goToSlide();
+  };
+
+  // Element builders
+  // Dots - Add/remove css classes
+  const handleDotClick = function (e) {
+    // Guard clause - if clicked element is not a dot, return
+    const dot = e.target.closest('.dots__dot');
+    if (!dot) return;
+
+    // Reassign current slide index to match clicked dot index and call goToSlide function
+    curSlideIndex = Number(dot.dataset.slide);
+    goToSlide();
+  };
+
+  // Event handlers
+  // Click event listeners for arrows
+  leftArrow.addEventListener('click', slideLeft);
+  rightArrow.addEventListener('click', slideRight);
+
+  // Click event listener for dots
+  dotsContainer.addEventListener('click', e => handleDotClick(e));
+
+  // Key event listener for slide control
+  document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowRight') slideRight();
+    if (e.key === 'ArrowLeft') slideLeft();
   });
-};
 
-// Create dots for each slide
-const createDots = function () {
-  slides.forEach((_, i) =>
-    dotsContainer.insertAdjacentHTML(
-      'beforeend',
-      `<button class="dots__dot" data-slide="${i}"></button>`,
-    ),
-  );
-};
+  // Initialization function to set up slider component
+  function initSlider() {
+    createDots(); // Create dots for each slide
+    goToSlide(); // Set initial slide position
+  }
 
-// Slide control functions
-// Go right or left depending on the position argument and move slides accordingly
-const slideRight = function () {
-  curSlide++;
-  moveSlides('right');
-};
+  initSlider();
+}
 
-const slideLeft = function () {
-  curSlide--;
-  moveSlides('left');
-};
-
-// Initial states
-moveSlides('start');
-createDots();
-
-// Event listening
-// Click event listeners for arrows
-leftArrow.addEventListener('click', slideLeft);
-rightArrow.addEventListener('click', slideRight);
-dotsContainer.addEventListener('click', )
-
-// Key event listener for slide control
-document.addEventListener('keydown', e => {
-  if (e.key === 'ArrowRight') slideRight();
-  if (e.key === 'ArrowLeft') slideLeft();
-});
-
-// Dots - Add/remove css classes
+slider();
